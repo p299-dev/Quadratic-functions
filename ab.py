2427e7bae9b9fd0ffea1d9464b91328497ccc719
p299-dev <121791365+p299-dev@users.noreply.github.com>
2024-01-12 16:47:02 +0800

    Update ab.py

--- ab.py
+++ ab.py
@@ -1,36 +1,36 @@
 import turtle as t
 x=0
 y=0
-a=float(input("二次项系数"))
-b=float(input("一次项系数"))
-c=float(input("常数项"))
+a=float(input("Quadratic term coefficients"))
+b=float(input("Primary term coefficients"))
+c=float(input("Constant terms"))
 t.screensize(1,1)
 t.speed('fastest')
 t.ht()
 t.up()
 t.goto(0,0)
 t.down()
-while y<=200 and y>=-200:#图像限制范围
-    y=a*(x**2)+b*x+c#表达式
-    t.goto(x,y)#图像放大倍数
-    x=x+0.05#图像精度
+while y<=200 and y>=-200:#Image Limit Range
+    y=a*(x**2)+b*x+c#expression
+    t.goto(x,y)
+    x=x+0.05
 t.up()
 t.goto(0,0)
 t.down()
 y=0
 x=0
-while y>=-200 and y<=200:#图像限制范围
-    y=a*(x**2)+b*x+c#表达式
-    t.goto(x,y)#图像放大倍数
-    x=x-0.05#图像精度
-#绘制x和y轴
+while y>=-200 and y<=200:#Image Limit Range
+    y=a*(x**2)+b*x+c#expression
+    t.goto(x,y)
+    x=x-0.05
+#Draw the x and y axes
 t.up()
 t.goto(0,0)
 t.down()
 t.pencolor("red")
 t.fd(300)
 t.lt(180)
-t.write('x',font=("宋体",40,"normal"))
+t.write('x',font=(40,"normal"))
 t.fd(600)
 t.lt(180)
 t.fd(300)
@@ -38,6 +38,6 @@ t.pencolor("blue")
 t.lt(90)
 t.fd(300)
 t.lt(180)
-t.write('y',font=("宋体",40,"normal"))
+t.write('y',font=(40,"normal"))
 t.fd(600)
 t.done()
